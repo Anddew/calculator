28410aec8c39501ae1e2e70b13477cdf55f1cf64
Andrey Bylinovich <anddew90@gmail.com>
2017-10-12 11:12:31 +0300

    Add test

---
+++ src/test/main/calculator/CalculatorTest.java
@@ -0,0 +1,25 @@
+package main.calculator;
+
+//import org.junit.After;
+//import org.junit.Before;
+//import org.junit.Test;
+
+//import static org.junit.Assert.*;
+
+//public class CalculatorTest {
+//    @Before
+//    public void setUp() throws Exception {
+//
+//    }
+//
+//    @After
+//    public void tearDown() throws Exception {
+//
+//    }
+//
+//    @Test
+//    public void startCalculator() throws Exception {
+//
+//    }
+//
+//}
